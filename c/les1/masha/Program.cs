c5b9e3ae147223d2574adb83fca8d2e5dac3de5d
nikw <divanchmo@gmail.com>
2022-12-06 21:58:40 +0300

    check name

---
+++ c/les1/masha/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Input username: ");
+string name = Console.ReadLine();
+if name == "Masha" {
+    Console.WriteLine("hooray");
+
+}
+else {
+    Console.Write("hi, ");
+    Console.WriteLine(name);
+    }
+
