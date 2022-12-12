371428937d9003d928dd9514e6364cb9392d5e9e
nikw <divanchmo@gmail.com>
2022-12-12 16:54:21 +0300

    task13 fixed

--- c/homework2/task13/Program.cs
+++ c/homework2/task13/Program.cs
@@ -4,11 +4,14 @@
 // Console.WriteLine(num);    //show me num
 
 //USER INPUT
-int num = int.Parse(Console.ReadLine()??"0");
+int num = int.Parse(Console.ReadLine() ?? "0");
 
 
 
-
-int temp = num; //will be num without last digit
-while (temp/1000 != 0) temp = temp/10;
-Console.WriteLine(temp%10);
+if (num < 100) Console.WriteLine("this number doesn't contain the third digit");
+else
+{
+    int temp = num; //will be num without last digit
+    while (temp / 1000 != 0) temp = temp / 10;
+    Console.WriteLine(temp % 10);
+}
