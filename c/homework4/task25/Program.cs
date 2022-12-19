77acb00add0e4e890b30411892e379ae1684559f
nick <divanchmo@gmail.com>
2022-12-19 18:55:47 +0300

    task 25, 27 done

---
+++ c/homework4/task25/Program.cs
@@ -0,0 +1,23 @@
+﻿int ReadData(string msg) //преобразует строку в число
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+void PrintData(string msg, double result) //выводит сообщение и вычисленный результат
+{
+    Console.WriteLine(msg + result);
+}
+double Pow(int num, int pow) //вычисляет число в степени
+{
+    double res = Math.Pow(num, pow);
+    return res;
+}
+
+
+PrintData("power is: ", Pow(ReadData("input your number"), (ReadData("input pow"))));
+
+// int num = ReadData("input your number");
+// int pow = ReadData("input pow");
+// PrintData("power is: ", Pow(num, pow));
+
+//вопрос: какая из записей более правильная? какая проще читается?
---
+++ c/homework4/task27/Program.cs
@@ -0,0 +1,19 @@
+﻿int ReadData(string msg) //преобразует строку в число
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+void PrintData(string msg, int result) //выводит сообщение и вычисленный результат
+{
+    Console.WriteLine(msg + result);
+}
+int CalculateSum (int num) {        //вычисляет сумму цифр
+    int sum = 0;
+    while (num>0) {
+        sum += num%10;
+        num /= 10;
+    }
+    return sum;
+}
+//char [] number = ReadData("input your number").toCharArray;
+PrintData("sum of your number's digits is ", CalculateSum(ReadData("input your number")));
