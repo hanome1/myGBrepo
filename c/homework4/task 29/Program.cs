148587c1d2118b9fe39794daf7d5972edc872407
nickLaptop <divanchmo@gmail.com>
2022-12-19 20:00:46 +0300

    homework4 done

--- c/homework4/task 29/Program.cs
+++ c/homework4/task 29/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+void PrintData(string res, int[] arr)
+{
+    Console.WriteLine(res);
+    PrintArr(arr);
+}
+
+int[] GenArr(int len, int bottomLimit, int topLimit)
+{
+    Random rnd = new Random();
+    int[] arr = new int[len];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(bottomLimit, topLimit+1);
+    }
+    return arr;
+}
+
+void PrintArr(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length-1; i++)
+    {
+        Console.Write(arr[i]+", ");
+    }
+    Console.WriteLine(arr[arr.Length-1]+"]");
+}
+
+// int arrLen = ReadData("Введите длину массива.");
+// int arbot = 3;
+// int artop = 5;
+// int[] arr = GenArr(arrLen, arbot, artop);
+
+// PrintData("Сгененрированный массив:", arr);
+
+PrintData("Сгененрированный массив:", GenArr(ReadData("Введите длину массива."), 
+                                            ReadData("Введите нижний предел."),
+                                            ReadData("Введите нижний предел.")));
--- c/homework4/task27/Program.cs
+++ c/homework4/task27/Program.cs
@@ -7,13 +7,30 @@ void PrintData(string msg, int result) //выводит сообщение и в
 {
     Console.WriteLine(msg + result);
 }
-int CalculateSum (int num) {        //вычисляет сумму цифр
-    int sum = 0;
+double CalculateSum (int num) {        //вычисляет сумму цифр
+    double sum = 0;
     while (num>0) {
         sum += num%10;
         num /= 10;
     }
     return sum;
 }
-//char [] number = ReadData("input your number").toCharArray;
-PrintData("sum of your number's digits is ", CalculateSum(ReadData("input your number")));
+
+//HARD PART
+
+int CalculateSumThroughArray (int num) {
+    int[] arr = num.ToIntArray();
+    
+    return arr;
+}
+
+int number = ReadData("input your number");
+DateTime d1 = DateTime.Now;
+int length1 = CalculateSum(number);
+Console.WriteLine(DateTime.Now - d1);
+
+DateTime d2 = DateTime.Now;
+int length2 = CalculateSumThroughArray(number);
+Console.WriteLine(DateTime.Now - d2);
+
+PrintData("sum of your number's digits is ", CalculateSum(number));
