22ce1ad2b5250a9794cc4b28a5260891586835a4
nick <divanchmo@gmail.com>
2022-12-22 13:00:22 +0300

    homework6 done

--- c/homework6/task41/Program.cs
+++ c/homework6/task41/Program.cs
@@ -1,15 +1,87 @@
-﻿string ReadString(string str) //чтение данных, перевод в число
+﻿// string ReadString(string str) //чтение данных, перевод в число
+// {
+//     Console.WriteLine(str);
+//     return (Console.ReadLine() ?? "0");
+// }
+// int ReadInt(string str)
+// {
+//     return int.Parse(ReadString(str));
+// }
+// void PrintString(string str) //вывод строки
+// {
+//     Console.Write(str);
+// }
+// void PrintArr(int[] arr)  //печатаем массив красиво
+// {
+//     Console.Write("\n[");
+//     for (int i = 0; i < arr.Length - 1; i++)
+//     {
+//         Console.Write(arr[i] + ", ");
+//     }
+//     Console.WriteLine(arr[arr.Length - 1] + "]");
+// }
+// int[] ArrayBuilder(int firstNum) //строим массив исходя из первого элемента
+// {
+//     int[] arr = {}; //создали пустой массив
+//     arr[0] = firstNum; //задали нулевой элемент
+//     int i = 0; //переменная индекса элемента
+//     string answ = ""; //строка ответа
+//     do 
+//     {
+//         answ = ReadString("Добавить еще элемент? (Да/Нет)");
+//         if (answ.ToLower() == "да")
+//         {
+//             i++;
+//             arr[i] = ReadInt("Введите новый элемент"); //достраиваем массив
+//         }
+//     }
+//     while (answ.ToLower() != "нет");
+//     return arr;
+// }
+// bool EvenTest(int num) //проверка на четность
+// {
+//     if (num % 2 == 0) return true;
+//     else return false;
+// }
+// string Counter(int[] arr) //счетчик четных элементов
+// {
+//     int count = 0;
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if (EvenTest(arr[i])) count++;
+//     }
+//     return count.ToString();
+// }
+
+// int[] userArray = ArrayBuilder(ReadInt("Введите первый элемент массива"));
+// PrintArr(userArray);
+// PrintString("Четных элементов в массиве: ");
+// PrintString(Counter(userArray));
+
+
+int ReadData(string msg) //чтение данных, перевод в число
 {
-    Console.WriteLine(str);
-    return (Console.ReadLine() ?? "0");
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
 }
-int ReadInt(string str)
+void PrintData(string res, int number) //вывод массива, сообщения и числа
 {
-    return int.Parse(ReadString(str));
+    Console.WriteLine(res + number);
 }
-void PrintString(string str) //вывод строки
+int CountPositive(int n) //считает положительные числа в массиве и печатает его. требует указать кол-во элементов (n)
 {
-    Console.Write(str);
+    int count = 0; 
+    int[] arr = new int[n];
+    int i = 0; //индекс
+    while (n>0)
+    {
+        arr[i] = ReadData("Введите число");
+        if (arr[i] > 0) count++;
+        i++;
+        n--;
+    }
+    PrintArr(arr);
+    return count;
 }
 void PrintArr(int[] arr)  //печатаем массив красиво
 {
@@ -20,42 +92,5 @@ void PrintArr(int[] arr)  //печатаем массив красиво
     }
     Console.WriteLine(arr[arr.Length - 1] + "]");
 }
-int[] ArrayBuilder(int firstNum) //строим массив исходя из первого элемента
-{
-    int[] arr = {}; //создали пустой массив
-    arr[0] = firstNum; //задали нулевой элемент
-    int i = 0; //переменная индекса элемента
-    string answ = ""; //строка ответа
-    do 
-    {
-        answ = ReadString("Добавить еще элемент? (Да/Нет)");
-        if (answ.ToLower() == "да")
-        {
-            i++;
-            arr[i] = ReadInt("Введите новый элемент"); //достраиваем массив
-        }
-    }
-    while (answ.ToLower() == "нет");
-    return arr;
-}
-bool EvenTest(int num) //проверка на четность
-{
-    if (num % 2 == 0) return true;
-    else return false;
-}
-string Counter(int[] arr) //счетчик четных элементов
-{
-    int count = 0;
-    for (int i = 0; i < arr.Length; i++)
-    {
-        if (EvenTest(arr[i])) count++;
-    }
-    return count.ToString();
-}
 
-int[] userArray = ArrayBuilder(ReadInt("Введите первый элемент массива"));
-PrintArr(userArray);
-PrintString("Четных элементов в массиве: ");
-PrintString(Counter(userArray));
-
-
+PrintData("\nположительных чисел в массиве: ", CountPositive(ReadData("Введите длину массива")));
---
+++ c/homework6/task43/Program.cs
@@ -0,0 +1,24 @@
+﻿int ReadData(string msg) {
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine()??"0");
+} 
+void PrintData(string res) {
+    Console.WriteLine(res);
+}
+string CrossFinder(int k1, int b1, int k2, int b2) {
+    string msg = "";
+    if (k1 == k2) msg = "Прямые не пересекутся";
+    else
+    {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = (k1 * b2 - k2 * b1) / (k1 - k2);
+        msg = $"Прямые пересекутся в точке ({x}; {y})";
+    }
+    return msg;
+}
+// int k1 = ReadData("введите k1");
+// int b1 = ReadData("введите k1");
+// int k2 = ReadData("введите k2");
+// int b2 = ReadData("введите b2");
+PrintData(CrossFinder(ReadData("введите k1"), ReadData("введите b1"), 
+                        ReadData("введите k2"), ReadData("введите b2")));
