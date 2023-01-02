caeaa019d32e21e11b8a057ea74b1b368d0d78b0
nickLaptop <divanchmo@gmail.com>
2023-01-02 19:39:28 +0300

    recursion is realized

---
+++ c/sem9/task67/Program.cs
@@ -0,0 +1,30 @@
+﻿
+// ---------------------------------------------------------------------
+//                                 Задача 64
+// Задайте значение N. Напишите программу, которая выведет все натуральные числа в 
+// промежутке от N до 1. Выполнить с помощью рекурсии.
+// ---------------------------------------------------------------------
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Печать результата
+void PrintResult(int prefix)
+{
+    Console.WriteLine(prefix);
+}
+
+int LineGenRec(int num)
+{
+    if (num % 10 == 0) return num;
+    else return num % 10 + LineGenRec(num/10);
+}
+
+int number = ReadData("Введите число N: ");
+int resultLine = LineGenRec(number);
+PrintResult(resultLine);
---
+++ c/sem9/task69/Program.cs
@@ -0,0 +1,23 @@
+﻿int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Печать результата
+void PrintResult(int prefix)
+{
+    Console.WriteLine(prefix);
+}
+
+int RecMpowN(int M, int N)
+{
+    if (N == 1) return M;
+    else return M * RecMpowN(M, N-1);
+}
+
+int number1 = ReadData("Введите число M: ");
+int number2 = ReadData("Введите число N: ");
+// string resultLine = number1<number2? RecMN(number1, number2):RecMN(number2, number1);
+PrintResult(RecMpowN(number1, number2));
